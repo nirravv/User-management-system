87444f7d889c065d6713493ce1c32d154bd5bc5e
Nirav <chaudharinirav4@gmail.com>
2022-06-30 21:57:50 -0400

    changed localhost url

--- server/services/render.js
+++ server/services/render.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
-    axios.get('http://localhost:3000/api/users')
+    axios.get('https://ums-express.vercel.app/api/users')
     .then(function(response){
         res.render('index', {users: response.data});
     })
@@ -18,7 +18,7 @@ exports.addUser = (req, res) => {
 
 exports.updateUser = (req, res) => {
     // Rendering the data into form
-    axios.get('http://localhost:3000/api/users', {params: { id : req.query.id}})
+    axios.get('https://ums-express.vercel.app/api/users', {params: { id : req.query.id}})
     .then(function(userdata){
         console.log(userdata);
         res.render('update_user', {user:userdata.data});
